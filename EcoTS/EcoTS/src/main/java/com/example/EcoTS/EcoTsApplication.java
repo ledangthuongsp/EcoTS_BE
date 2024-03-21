cc0e3ccb155abe414235db4f149c7c64f6324e97
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-03-21 14:06:07 +0700

    Create new Project

---
+++ EcoTS/EcoTS/src/main/java/com/example/EcoTS/EcoTsApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTsApplication.class, args);
+	}
+
+}
---
+++ EcoTS/EcoTS/src/test/java/com/example/EcoTS/EcoTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
