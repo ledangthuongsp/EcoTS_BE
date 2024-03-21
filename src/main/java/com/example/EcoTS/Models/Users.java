5c5d4b4fdaafaf56b3f614de6c533f4f94fcd18b
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-03-21 14:19:38 +0700

    Khởi tạo table Users

--- src/main/java/com/example/EcoTS/Models/Users.java
+++ src/main/java/com/example/EcoTS/Models/Users.java
@@ -1,4 +1,17 @@
 package com.example.EcoTS.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
