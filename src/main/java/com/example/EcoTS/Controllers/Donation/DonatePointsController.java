060f740a1d5ba592b8047d9da371c432c762af2e
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-04-29 11:59:54 +0700

    Fix lỗi

--- src/main/java/com/example/EcoTS/Controllers/Donation/DonatePointsController.java
+++ src/main/java/com/example/EcoTS/Controllers/Donation/DonatePointsController.java
@@ -2,12 +2,14 @@ package com.example.EcoTS.Controllers.Donation;
 
 import com.example.EcoTS.Repositories.DonationRepository;
 import com.example.EcoTS.Services.DonationService.DonationService;
+import com.example.EcoTS.Services.SecurityService.JwtService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,11 +26,16 @@ public class DonatePointsController {
     private DonationRepository donationRepository;
     @Autowired
     private DonationService donationService;
+    @Autowired
+    private JwtService jwtService;
     @PostMapping("/donatePoints")
-    public ResponseEntity<String> donatePoints(@PathVariable Long donationId,
-                                               @RequestParam Long userId,
+    public ResponseEntity<String> donatePoints(@RequestHeader("Authorization") String token,
+                                               @PathVariable Long donationId,
                                                @RequestParam double points) {
-        donationService.donatePoints(userId, donationId, points);
+        // Giải mã token và lấy username
+        String username = jwtService.getUsername(token);
+        // Thực hiện donatePoints
+        donationService.donatePoints(username, donationId, points);
         return ResponseEntity.ok("Points donated successfully");
     }
 }
