6698595847c061bd6661a3e8fd6b6af06d32bab2
IDS-V-Thuongld <thuongld@vn.ids.jp>
2025-07-02 23:40:51 +0700

    save all

--- src/main/java/com/example/EcoTS/Controllers/Reward/RewardItemLocationController.java
+++ src/main/java/com/example/EcoTS/Controllers/Reward/RewardItemLocationController.java
@@ -2,6 +2,7 @@ package com.example.EcoTS.Controllers.Reward;
 
 import com.example.EcoTS.DTOs.Response.RewardItemStockResponse;
 import com.example.EcoTS.Services.RewardItem.RewardItemLocationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reward/employee")
+@RequestMapping("/reward")
 @RequiredArgsConstructor
+@Tag(name = "Reward Import API")
 public class RewardItemLocationController {
 
     @Autowired
@@ -21,7 +23,7 @@ public class RewardItemLocationController {
      * GET /api/reward-items/location/{locationId}/stock
      * Trả về danh sách reward item và số lượng tồn kho tại địa điểm
      */
-    @GetMapping("/get-location-stock")
+    @GetMapping("/employee/get-location-stock")
     public ResponseEntity<List<RewardItemStockResponse>> getRewardItemsByLocation(@PathVariable Long locationId) {
         List<RewardItemStockResponse> response = rewardItemLocationService.getStockByLocation(locationId);
         return ResponseEntity.ok(response);
