cf83eca10b5458546310561b856558189d46b510
IDS-V-Thuongld <thuongld@vn.ids.jp>
2025-07-04 14:48:59 +0700

    save reward item

--- src/main/java/com/example/EcoTS/Controllers/Reward/RewardController.java
+++ src/main/java/com/example/EcoTS/Controllers/Reward/RewardController.java
@@ -19,25 +19,17 @@ import java.util.Optional;
 public class RewardController {
     @Autowired
     private RewardItemService rewardItemService;
-
-    @GetMapping
+    @GetMapping("/get-all-reward")
     public List<RewardItem> getAllRewards() {
         return rewardItemService.getAllRewards();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<RewardItem> getRewardById(@PathVariable Long id) {
-        Optional<RewardItem> rewardItem = rewardItemService.getRewardById(id);
-        return rewardItem.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @PostMapping
+    @PostMapping("/add-new-reward")
     public RewardItem createReward(@RequestBody RewardItem rewardItem) {
         return rewardItemService.createReward(rewardItem);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update-reward/{id}")
     public ResponseEntity<RewardItem> updateReward(@PathVariable Long id, @RequestBody RewardItem updatedReward) {
         try {
             RewardItem rewardItem = rewardItemService.updateReward(id, updatedReward);
@@ -47,40 +39,10 @@ public class RewardController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete-reward/{id}")
     public ResponseEntity<Void> deleteReward(@PathVariable Long id) {
         rewardItemService.deleteReward(id);
         return ResponseEntity.noContent().build();
-    }
-
-    @PutMapping("/update-reward-history")
-    @Operation(summary = "Update Reward History", description = "Tự động cập nhật điểm, trừ stock, lưu lại lịch sử nếu điều kiện hợp lệ.")
-    public ResponseEntity<String> updateRewardHistory(
-            @RequestParam Long userId,
-            @RequestParam double point,
-            @RequestParam Long rewardItemId,
-            @RequestParam Long numberOfItem,
-            @RequestParam Long locationId) {
-
-        try {
-            rewardItemService.updateHistoryCharge(userId, point, rewardItemId, numberOfItem, locationId);
-            return ResponseEntity.ok("Reward history updated successfully.");
-        } catch (IllegalArgumentException ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        } catch (RuntimeException ex) {
-            return ResponseEntity.status(500).body("Internal server error: " + ex.getMessage());
-        }
-    }
-
-    @GetMapping("/get-reward-history")
-    public ResponseEntity<List<RewardHistory>> getRewardHistory(@RequestParam Long userId) {
-        List<RewardHistory> rewardHistory = rewardItemService.getAllRewardHistoryById(userId);
-
-        if (rewardHistory == null || rewardHistory.isEmpty()) {
-            return ResponseEntity.notFound().build(); // Trả về 404 nếu không tìm thấy dữ liệu
-        }
-
-        return ResponseEntity.ok(rewardHistory); // Trả về 200 với danh sách dữ liệu
     }
 
 }
